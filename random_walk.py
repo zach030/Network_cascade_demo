e1412fbe1098d460de369269a642878aed4e71d5
Zach <zachchou016@gmail.com>
2020-10-06 11:31:57 +0800

    [add] add one node random_walk

--- random_walk.py
+++ random_walk.py
@@ -1,33 +1,8 @@
 import random
-
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-
-
-# try this graph!!
-# newman watts
-#
-
-# def barabassi_graph_andre(n, m):
-#     G = nx.Graph()
-#     G.add_edges_from([(1, 2), (2, 3), (1, 3)])
-#     counter = 0
-#     prob_list = []
-#     for i in range(4, n):
-#         G.add_node(i)
-#     for node in range(4, n):
-#         for node_iter in range(1, node):
-#             for node_multiply in range(G.degree(node_iter)):
-#                 prob_list.append(node_iter)
-#         while not counter == m:
-#             G.add_edge(node, int(random.choice(prob_list)))
-#             counter += 1
-#         counter = 0
-#         prob_list = []
-#     return G
-
 
 def random_weighted_walk(G, initial_node, steps):
     walk_list = [initial_node]
